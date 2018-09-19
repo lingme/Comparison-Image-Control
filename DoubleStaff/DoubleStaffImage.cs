af26bb0004573381159d81ca740c24c287d09e11
e <kid--l@hotmail.com>
2018-09-19 11:22:04 +0800

    增加交替图片显示空间，基于.net WPF

---
+++ DoubleStaff/DoubleStaffImage.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoubleStaff
+{
+    [TemplatePart(Name = "PART_Left",Type = typeof(Grid))]
+    public class DoubleStaffImage : Control , INotifyPropertyChanged
+    {
+        private Grid _GridLeft;
+
+        /// <summary>
+        /// 底图
+        /// </summary>
+        public ImageSource BottomImage
+        {
+            get { return (ImageSource)GetValue(BottomImageProperty); }
+            set { SetValue(BottomImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty BottomImageProperty = DependencyProperty.Register(
+            "BottomImage", 
+            typeof(ImageSource), 
+            typeof(DoubleStaffImage), 
+            new PropertyMetadata(default(ImageSource)));
+
+        /// <summary>
+        /// 叠加图
+        /// </summary>
+        public ImageSource TopImage
+        {
+            get { return (ImageSource)GetValue(TopImageProperty); }
+            set { SetValue(TopImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty TopImageProperty =DependencyProperty.Register(
+            "TopImage", 
+            typeof(ImageSource), 
+            typeof(DoubleStaffImage), 
+            new PropertyMetadata(default(ImageSource)));
+
+        private Rect _RectLeft;
+        public Rect RectLeft { get { return _RectLeft; } set { _RectLeft = value; NotifyChanged(() => RectLeft); } }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            if((_GridLeft = GetTemplateChild("PART_Left") as Grid) != null)
+            {
+                _GridLeft.SizeChanged += _GridLeft_SizeChanged;
+            }
+        }
+
+        private void _GridLeft_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if(sender is Grid grid)
+            {
+                RectLeft = new Rect(0, 0, grid.ActualWidth + 5, grid.ActualHeight);
+            }
+        }
+
+        static DoubleStaffImage()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DoubleStaffImage), new FrameworkPropertyMetadata(typeof(DoubleStaffImage)));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyChanged<T>(Expression<Func<T>> propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                var memberExpression = propertyName.Body as MemberExpression;
+                if (memberExpression != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(memberExpression.Member.Name));
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Alternate-Image-Control-WPF
+# WPF 交替图片显示控件
+---
 WPF实现的图片交替显示控件，可以叠加两层。
+
+##### 命名空间引入：
+```C {.line-numbers}
+xmlns:control="clr-namespace:DoubleStaff;assembly=DoubleStaff"
+```
+
+##### 使用：
+
+```C {.line-numbers}
+<control:DoubleStaffImage 
+        BottomImage="Image/bottom.jpg" 
+        TopImage="Image/top.jpg">
+</control:DoubleStaffImage>
+```
+
+##### 属性：
+
+| 属 性        | 类 型   |  说 明  |
+| --------   | -----  | ----  |
+|BottomImage|ImageSource|底图|
+|TopImage|ImageSource|叠加图|
+
+##### 效果
+![fdfd](https://github.com/lingme/Picture_Bucket/raw/master/Alternate_Image_Img/AlternateGif.gif)
